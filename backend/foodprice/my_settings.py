594b58d207a2f508c6d6bce0d8cc757b859d6cff
Ubuntu <ubuntu@ip-172-31-9-13.ap-northeast-2.compute.internal>
2021-10-13 14:27:19 +0000

    init backend

---
+++ backend/foodprice/my_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'foodprice',
+        'USER': 'jinpyo',
+        'PASSWORD': 'dnleogks771',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
--- backend/foods/urls.py
+++ backend/foods/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    path('', views.graph, name='graph')
+    path('', views.)
 ]
