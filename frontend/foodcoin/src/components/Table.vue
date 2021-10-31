06dce37cb8fd0cb6a3ea2a30870a494c4b23a467
jinpyo <hojp7874@gmail.com>
2021-10-31 06:07:26 +0000

    food table

--- frontend/foodcoin/src/components/Table.vue
+++ frontend/foodcoin/src/components/Table.vue
@@ -7,16 +7,12 @@
 <script>
 export default {
   name: 'Table',
-  data() {
+  data: function() {
     return {
-      items: [
-        { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
-        { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
-        { age: 89, first_name: 'Geneva', last_name: 'Wilson' },
-        { age: 38, first_name: 'Jami', last_name: 'Carney' }
-      ]
+      foods: this.items
     }
-  }
+  },
+  props: ['items']
 }
 </script>
 
--- frontend/foodcoin/src/views/FoodChart.vue
+++ frontend/foodcoin/src/views/FoodChart.vue
@@ -1,19 +1,49 @@
 <template>
   <div>
     <chart-page/>
-    <Table/>
+    <Table :items="items"/>
   </div>
 </template>
 
 <script>
 import ChartPage from '../components/ChartPage.vue'
 import Table from '../components/Table.vue'
+import axios from 'axios'
+
+const SERVER_URL = process.env.VUE_APP_SERVER_URL_LOCAL
 
 export default {
   name: 'FoodChart',
   components: {
     Table,
     ChartPage
+  },
+  data: function() {
+    return {
+      items: Object
+    }
+  },
+  // computed: {
+  //   foods: function() {
+  //     return 
+  //   }
+  // },
+  methods: {
+    get_foods: function() {
+      axios.get(`${SERVER_URL}/prices/foods/`)
+        .then((res) => {
+          console.log(res)
+          this.items = res.data
+          console.log(this.items)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+      return 'test'
+    }
+  },
+  created: function() {
+    this.get_foods()
   }
 }
 </script>
